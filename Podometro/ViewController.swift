2ac26f8847732e5b02ff5aa4d24766da32793466
Jorge Maldonado Borbón <jorge@2806-108e-0005-35c8-083b-99e9-2c16-6f93.ipv6.infinitum.net.mx>
2018-02-14 19:01:22 -0600

    Initial Commit

---
+++ Podometro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Podometro
+//
+//  Created by Jorge Maldonado Borbón on 14/02/18.
+//  Copyright © 2018 Jorge Maldonado Borbón. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
